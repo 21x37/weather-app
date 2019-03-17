6d2ed705a31dae1d9f5680508043c384c61a7842
21x37 <tylermok@outlook.com>
2019-03-16 20:08:48 -0700

    updated bio and forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined);
         } else {
-            const dataString = `${body.daily.data[0].summary} It is currently ${body.currently.temperature}. There is a ${body.currently.precipProbability}% chance of rain.`;
+            const dataString = `${body.daily.data[0].summary} It is currently ${body.currently.temperature}. The temperature high for today is ${body.daily.data[0].temperatureMax} with a low of ${body.daily.data[0].temperatureMin} There is a ${body.currently.precipProbability}% chance of rain.`;
             callback(undefined, dataString);
         };
     });
